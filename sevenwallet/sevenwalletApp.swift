977f7df9390563ad05eea07b3dbf8e02cd7bb197
Wane <wanewwa@gmail.com>
2026-07-22 16:42:15 +0800

    Initial Commit

---
+++ sevenwallet/sevenwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  sevenwalletApp.swift
+//  sevenwallet
+//
+//  Created by Wane on 2026/7/22.
+//
+
+import SwiftUI
+
+@main
+struct sevenwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sevenwalletTests/sevenwalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  sevenwalletTests.swift
+//  sevenwalletTests
+//
+//  Created by Wane on 2026/7/22.
+//
+
+import Testing
+@testable import sevenwallet
+
+struct sevenwalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
